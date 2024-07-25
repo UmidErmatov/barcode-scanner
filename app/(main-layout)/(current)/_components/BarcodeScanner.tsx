536b10ebf76e7bff8c20b8837a8a59187bbad81a
user <user@MacBook-Pro-user.local>
2024-07-25 11:11:45 +0500

    new scanner

--- app/(main-layout)/(current)/_components/BarcodeScanner.tsx
+++ app/(main-layout)/(current)/_components/BarcodeScanner.tsx
@@ -2,6 +2,7 @@
 "use client"
 import { useCommonStore } from '@/store/common';
 import { Html5Qrcode, Html5QrcodeCameraScanConfig } from 'html5-qrcode';
+import { Scanner } from '@yudiel/react-qr-scanner';
 import { useEffect, useRef, useState } from 'react';
 import './codeScannerStyle.css'
 import BarcodeDataForm from './BarcodeDataForm';
@@ -32,70 +33,71 @@ function BarcodeScanner({ }: Props) {
     const tableData = excelData && excelData.excelData && typeof excelData.excelData === 'object' &&
         Array.isArray(excelData.excelData) ? excelData.excelData as any[] : []
 
-    useEffect(() => {
-        const scanConfig: Html5QrcodeCameraScanConfig = {
-            fps: 100,
-            qrbox: {
-                width: 250,
-                height: 50,
-            },
+    // useEffect(() => {
+    //     const scanConfig: Html5QrcodeCameraScanConfig = {
+    //         fps: 100,
+    //         qrbox: {
+    //             width: 250,
+    //             height: 50,
+    //         },
 
-            // disableFlip: true,
-            // aspectRatio: 5
-        }
-        const html5QrCode = new Html5Qrcode("qrCodeContainer")
+    //         // disableFlip: true,
+    //         // aspectRatio: 5
+    //     }
+    //     const html5QrCode = new Html5Qrcode("qrCodeContainer")
 
-        const qrScannerStop = () => {
-            if (html5QrCode && html5QrCode.isScanning) {
-                html5QrCode.stop()
-                    .then(() => {
-                        console.log("Scanner stopped");
-                    })
-                    .catch(() => {
-                        console.log("Scanner error");
-                    })
-            }
-        }
+    //     const qrScannerStop = () => {
+    //         if (html5QrCode && html5QrCode.isScanning) {
+    //             html5QrCode.stop()
+    //                 .then(() => {
+    //                     console.log("Scanner stopped");
+    //                 })
+    //                 .catch(() => {
+    //                     console.log("Scanner error");
+    //                 })
+    //         }
+    //     }
 
-        const qrCodeSuccess = (result: string) => {
-            if (audioRef.current) {
-                audioRef.current.currentTime = 0;
-                audioRef.current.play();
-            }
+    //     const qrCodeSuccess = (result: string) => {
+    //         if (audioRef.current) {
+    //             audioRef.current.currentTime = 0;
+    //             audioRef.current.play();
+    //         }
 
-            const findProduct = tableData.find(excelProduct => excelProduct?.Barcode?.toString()?.split(",")?.map((code: string) => code.trim())?.includes(result))
-            if (findProduct) {
-                qrScannerStop()
-                setScanModalHeader("Ma'lumotlar")
-                setQrResult(findProduct ? { barcode: findProduct.Barcode.toString(), name: findProduct.Nomi, quantity: +findProduct.Miqdori, shelfLife: new Date(findProduct.Muddati), manufacturer: findProduct['Ishlab chiqaruvchi'], buyPrice: findProduct['Tan narxi'] } : { ...defaultBarcodeData, barcode: result })
-            } else {
-                toast({
-                    title: "Qutiga tashlang!",
-                    variant: 'destructive'
-                })
-                return
-            }
-        }
+    //         const findProduct = tableData.find(excelProduct => excelProduct?.Barcode?.toString()?.split(",")?.map((code: string) => code.trim())?.includes(result))
+    //         if (findProduct) {
+    //             qrScannerStop()
+    //             setScanModalHeader("Ma'lumotlar")
+    //             setQrResult(findProduct ? { barcode: findProduct.Barcode.toString(), name: findProduct.Nomi, quantity: +findProduct.Miqdori, shelfLife: new Date(findProduct.Muddati), manufacturer: findProduct['Ishlab chiqaruvchi'], buyPrice: findProduct['Tan narxi'] } : { ...defaultBarcodeData, barcode: result })
+    //         } else {
+    //             toast({
+    //                 title: "Qutiga tashlang!",
+    //                 variant: 'destructive'
+    //             })
+    //             return
+    //         }
+    //     }
 
-        if (openScannerModal) {
-            html5QrCode.start({ facingMode: "environment" }, scanConfig, qrCodeSuccess, () => {
-                // toast({
-                //     title: "Skanerlab bo'lmayapti"
-                // })
-            })
-            setQrResult(defaultBarcodeData)
-        } else {
-            qrScannerStop()
-        }
+    //     if (openScannerModal) {
+    //         html5QrCode.start({ facingMode: "environment" }, scanConfig, qrCodeSuccess, () => {
+    //             // toast({
+    //             //     title: "Skanerlab bo'lmayapti"
+    //             // })
+    //         })
+    //         setQrResult(defaultBarcodeData)
+    //     } else {
+    //         qrScannerStop()
+    //     }
 
-        return () => {
-            qrScannerStop()
-        }
-    }, [openScannerModal])
+    //     return () => {
+    //         qrScannerStop()
+    //     }
+    // }, [openScannerModal])
 
     return (
         <div className='px-4'>
             <audio ref={audioRef} src="/scanner-beep.mp3" preload="auto" hidden />
+            {/* <Scanner onScan={(result) => console.log(result)} formats={['aztec', 'codabar', 'code_128', 'code_39', 'data_matrix', 'databar', 'databar_expanded', 'dx_film_edge', 'ean_13', 'ean_8', 'qr_code', 'itf', 'linear_codes', 'matrix_codes', 'maxi_code', 'micro_qr_code', 'pdf417', 'rm_qr_code', 'unknown', 'upc_a', 'upc_e']} /> */}
             <div id='qrCodeContainer' className={cn(qrResult.barcode ? 'hidden' : '')} />
             {qrResult.barcode && <BarcodeDataForm defaultBarcodeData={{ ...qrResult, id: "" }} />}
         </div>
